89a415564ff3f39b0dcc0117e90278061b878d77
eliranmal <eliranmal@gmail.com>
2019-03-14 03:30:30 +0200

    wat!

---
+++ src/app.js
@@ -0,0 +1,73 @@
+(function (d) {
+
+  function paintCss(image = {}, paper = {}, {
+    x = 0,
+    y = 0,
+    width = image.width,
+    height = image.height,
+    blur = 0,
+  } = {}) {
+
+    const canvas = d.createElement('canvas');
+    canvas.width = width;
+    canvas.height = height;
+
+    canvas.getContext('2d')
+      .drawImage(image, 0, 0, width, height);
+
+    const pixelData = canvas.getContext('2d')
+      .getImageData(0, 0, width, height).data;
+    // console.log('>>> pixel data:', pixelData);
+
+    const chunkedData = chunks(4, pixelData);
+    // console.log('>>> chunked data:', chunkedData);
+
+    const rgbValues = chunkedData.map(pixelChunk => (
+      `rgb(${pixelChunk.join(',')})`
+    ));
+    // console.log('>>> rgb values:', rgbValues);
+
+    const offsetMatrixPoints = chunkedData.map((pixelChunk, i) => ({
+      x: i % width,
+      y: (i - (i % width)) / width,
+    }));
+    // console.log('>>> pixel offset matrix as points:',
+    // JSON.stringify(offsetMatrixPoints, null, 2));
+
+    const boxShadow = offsetMatrixPoints
+      .map((point, i) => `${point.x}px ${point.y}px ${blur}px ${rgbValues[i]}`)
+      .join(',');
+    // console.log('>>> box shadow:', boxShadow);
+
+    paper.style.boxShadow = boxShadow;
+    paper.style.position = 'relative';
+    paper.style.left = `calc(50% - ${width / 2}px)`;
+    paper.style.height = '1px';
+    paper.style.width = '1px';
+    paper.style.backgroundColor = 'transparent';
+  }
+
+  function chunks(chunkSize, arr) {
+    const chunked = [];
+    for (let c = 0; c < arr.length; c += chunkSize) {
+      chunked.push(arr.slice(c, c + chunkSize));
+    }
+    return chunked;
+  }
+
+
+  function entry() {
+    const img = d.getElementById('source-image');
+    const paper = d.getElementById('paper');
+
+    d.getElementById('paint-btn')
+      .addEventListener('click', () => paintCss(img, paper, {
+        width: 600,
+        height: 50,
+      }), false);
+  }
+
+
+  entry();
+
+}(document));
